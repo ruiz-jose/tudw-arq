6a577b75964a06a1da3d12973dd5105ebe71c4b2
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-16 08:17:31 -0300

    fix comment

--- semana1/python/01.py
+++ semana1/python/01.py
@@ -3,19 +3,19 @@
 # ejemplo para arquitectura de 64 bits
 
 # valor entero máximo en Python 3 Max Int
-   
-#  valor maximo entero 2^63 -1 = 9223372036854775807
-#  valor minimo entero   -2^63 =-9223372036854775808
+
+#  valor maximo entero 2**63 -1 = 9223372036854775807
+#  valor minimo entero   -2**63 =-9223372036854775808
 #  tipo de datos <class 'int'>
 
 import sys
 
-# valor maximo entero 2^63 -1
+# valor maximo entero 2**63 -1
 max_val = sys.maxsize
 print(max_val)
 
 
-# valor minimo entero -2^63
+# valor minimo entero -2**63
 min_val = - sys.maxsize - 1
 print(min_val)
 
