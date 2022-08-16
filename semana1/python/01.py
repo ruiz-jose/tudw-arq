722960fbc9926a3db9efb283ba7e8bbe3556cf45
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-16 08:00:23 -0300

    eje python

--- semana1/python/01.py
+++ semana1/python/01.py
@@ -1,7 +1,17 @@
 #https://www.online-python.com/
-# importing the module
+
+# ejemplo para arquitectura de 64 bits
+
+# valor entero máximo en Python 3 Max Int
 import sys
-       
-# fetching the maximum value
+
+
+# valor maximo entero 2^63 -1
 max_val = sys.maxsize
 print(max_val)
+print(type(max_val))
+
+# valor min entero -2^63
+min_val = - sys.maxsize - 1
+print(smin_val)
+
