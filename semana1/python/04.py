84df02ad522cb0f1f16f95b6a8381fb6624731a1
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-17 19:16:31 -0300

    Ejemplo 04

---
+++ semana1/python/04.py
@@ -0,0 +1,21 @@
+'''
+    Ejemplo 05.py:
+    Multiplicacion: el operador << desplaza todos los bit n unidades a la izquierda. 
+    division: el operador >> desplaza todos los bit n unidades a la derecha.
+   
+ '''
+# mover cada bit 3 unidades a la izquierda 
+x=0b1 # x= 1 = binario 0b1
+print(x)
+print(bin(x<<3)) # 1*2**3=8
+print(x<<3)
+
+# mover cada bit 2 unidades a la derecha  
+x=0b1000 # x= 8 = binario 0b10
+print(x)
+print(bin(x>>2)) # 8/2**2=2
+print(x>>2)
+
+
+
+
