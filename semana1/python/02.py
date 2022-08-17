8d35480b4776d0a89cc3a5691d1958ef7a3adfb1
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-17 13:44:25 -0300

    comentarios 2

--- semana1/python/02.py
+++ semana1/python/02.py
@@ -1,20 +1,31 @@
-# importing the module
+'''
+    Ejemplo 02.py muestra error al superar el rango de los valores enteros
+    probado para una arquitectura de 64 bits con python 3
+    valor entero máximo en Python 3 Max Int
+    - valor maximo entero 2**63 -1 = 9223372036854775807
+    - valor minimo entero   -2**63 =-9223372036854775808
+    - tipo de datos <class 'int'> 
+    
+    error: python int too large to convert to C ssize_t
+ '''
+
 import sys
 	
-# fetching the maximum value
 max_val = sys.maxsize
 
 try:
 
-	# creating list with max size + 1
-	list = range(max_val + 1)
 
-	# displaying the length of the list
+	max_val = max_val + 1
+	list = range(max_val)
+	
+	# Mostrar el tamaño de list
 	print(len(list))
-	print("List successfully created")
+	
+	print("max_val = max_val + 1 Exitoso")
 	
 except Exception as e:
 
 	# displaying the exception
 	print(e)
-	print("List creation unsuccessful")
+	print("max_val = max_val + 1 NO Exitoso")
