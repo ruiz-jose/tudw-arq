d7281fba14729079a0b23ea5b1bb078ace8db7ad
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-17 18:17:35 -0300

    coment

--- semana1/python/01.py
+++ semana1/python/01.py
@@ -1,36 +1,15 @@
 
 '''
-    Ejemplo 01.py: Muestra el rango maximo de elementos que pueden tener 
-    las estructuras de datos (listas, diccionarios, conjuntos, etc.) en python
-    esta dato por el valor entero máximo en Python 3 Max Int arquitectura x86-64 bits
-    - valor maximo entero 2**63 -1 = 9223372036854775807
-    - valor minimo entero -2**63 =-9223372036854775808
-    - tipo de datos <class 'int'>
-
-    En otros lenguajes de programación, los int tenían un valor máximo que pueden representar.
-    Con 64 bits, el rango es de -9.223.372.036.854.775.808 hasta 9.223.372.036.854.775.807.
+    Ejemplo 01.py: 
+    En otros lenguajes de programación los int tenían un valor máximo que pueden representar.
     En Python no tenemos que preocupar de esto, ya que el interprete de python 
-    asigna memoria dinamicamente al numero y podemos representar prácticamente cualquier número.
+    asigna memoria dinamicamente y el limite esta dado por la memoria disponible.
 
     https://www.onlinegdb.com/online_python_compiler
     https://www.online-python.com/
 '''
 
-# importar libreria de python sys y su atributo maxsize
-import sys
-
-# valor maximo entero 2**63 -1
-max_val = sys.maxsize
-print(max_val)
-
-
-# valor minimo entero -2**63
-min_val = - sys.maxsize - 1
-print(min_val)
-
-# tipo de datos de max_val
-print(type(max_val))
-
+# 250 elevado 250 
 x = 250**250
 print(x)
 print(type(x))
--- semana1/python/02.py
+++ semana1/python/02.py
@@ -1,20 +1,40 @@
 '''
-    Ejemplo 02.py: Muestra error que es atrapada por una excepcion 
-	cuando se supera el rango de valores de una lista.
-    Probado para una arquitectura de 64 bits con python 3,
+    Ejemplo 02.py:
+	Muestra el rango maximo de elementos que pueden tener 
+    las estructuras de datos (listas, diccionarios, conjuntos, etc.) en python
+    esta dato por el valor entero máximo.
+	 Probado para una arquitectura de 64 bits con python 3,
     valor entero máximo en Python 3 Max Int
     - valor maximo entero 2**63 -1 = 9223372036854775807
     - valor minimo entero -2**63 =-9223372036854775808
     - tipo de datos <class 'int'> 
-    
+
+	Si se genera una estructura (Lista) con un rango superior
+	y se intenta acceder a la cantidad de elementos de la lista (len)
+	genera una excepcion que es atrapada.
+       
     error: python int too large to convert to C ssize_t
 
 	El valor sys.maxsize informa el tamaño maximo de una estructura de datos en Python.
  '''
 
+
+
+# importar libreria de python sys y su atributo maxsize
 import sys
-	
+
+# valor maximo entero 2**63 -1
 max_val = sys.maxsize
+print(max_val)
+
+
+# valor minimo entero -2**63
+min_val = - sys.maxsize - 1
+print(min_val)
+
+# tipo de datos de max_val
+print(type(max_val))
+
 
 try:
 
@@ -32,6 +52,6 @@ try:
 	
 except Exception as e:
 
-	# Se atraba el error
+	# Se atrapa el error
 	print(e)
 	print("NO Exitoso supero tamaño de la lista")
