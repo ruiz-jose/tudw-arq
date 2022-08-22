9c8269fa1e96b8e2c3f8721582b8a757ba205a7a
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-21 21:55:30 -0300

    Create 02.py

---
+++ semana2/02.py
@@ -0,0 +1,21 @@
+import time
+
+# get the start time
+st = time.process_time()
+
+# main program
+# find sum to first 1 million numbers
+sum_x = 0
+for i in range(1000000):
+    sum_x += i
+
+# wait for 3 seconds
+time.sleep(3)
+print('Sum of first 1 million numbers is:', sum_x)
+
+# get the end time
+et = time.process_time()
+
+# get execution time
+res = et - st
+print('CPU Execution time:', res, 'seconds')
