e04455d551f2b0faebf9ac0d73aea17c13160c43
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-08-23 10:42:19 -0300

    fix ejemplo

--- semana2/02.c
+++ semana2/02.c
@@ -1,13 +1,16 @@
 /******************************************************************************
  ejemplo 02: programa para utilizar con el comando time linux (bash)
 El tiempo de ejecucion de un programa en una computadora se descompone en:
-- 'real'es el tiempo del reloj de pared: el tiempo desde el principio hasta el final de la llamada.
-  Es el tiempo total transcurrido o tiempo de respuesta del programa, incluidos los intervalos de tiempo
-   utilizados por otros procesos y el tiempo que el proceso pasa bloqueado (por ejemplo, si está esperando que se complete la E/S).
-- 'user' es el tiempo del CPU dedicado al código en modo usuario (fuera del kernel) dentro del proceso.
-   Este es solo el tiempo de CPU real utilizado para ejecutar el proceso. Otros procesos y el tiempo que el proceso pasa bloqueado no cuentan para esta cifra.
-- 'sys' es el tiempo de CPU invertido en el kernel dentro del proceso. Esto significa que el tiempo de CPU dedicado a las llamadas al sistema dentro del kernel.
-   Al igual que 'user', este es solo el tiempo de CPU utilizado por el proceso. 
+**real** es el tiempo desde el principio hasta el final de ejecucion del programa, 
+   como si se hubiera medido con un cronómetro o reloj de pared. 
+   Tambien se denomina tiempo transcurrido o tiempo de respuesta del programa,
+   incluye los intervalos de tiempo utilizados por otros procesos y 
+   el tiempo que el proceso pasa bloqueado (por ejemplo, si está esperando que se complete la E/S).
+- **user** es el tiempo del CPU dedicado al código en modo usuario (fuera del kernel).
+    Es el tiempo de CPU realmente utilizado para ejecutar el proceso. 
+	 Otros procesos y el tiempo que el proceso pasa bloqueado no cuentan para esta cifra.
+- **sys** es el tiempo de CPU invertido en el kernel. 
+    Esto significa que el tiempo de CPU dedicado a las llamadas al sistema dentro del kernel.
 *******************************************************************************/
 
 
@@ -21,7 +24,14 @@ El tiempo de ejecucion de un programa en una computadora se descompone en:
 
 int main()
 {
-	// duerme 3 segundos el programa
+	
+   // Calcular la suma del primer millon de numeros (0-999999)
+   int suma=0;
+   for(int i=0;i< 1000000;i++){
+         suma += i;
+   }
+   printf("'La suma del primer millon de números es: %d\n",suma);
+   // duerme 3 segundos el programa
 	sleep(3);
 	return 0;
 }
