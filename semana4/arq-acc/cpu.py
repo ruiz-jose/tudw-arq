0e2fa8f745a0fee8de021f4824d222585c05b1ea
ruiz-jose <jose.ruiz@uner.edu.ar>
2022-09-17 09:04:51 -0300

    .

--- semana4/arq-acc/cpu.py
+++ semana4/arq-acc/cpu.py
@@ -2,6 +2,13 @@ import sys
 from assembly import ASSEMBLY
 
 
+'''
+- Clase CLOCK, 
+    - Atributos - ciclos
+    - Metodos:
+        CountCycles - suma 1 ciclo
+        WaitCycles  - suma 4 ciclos
+'''
 class CLOCK:
     def __init__(self):
         self.cycles = 0
@@ -12,33 +19,43 @@ class CLOCK:
     def WaitCycles4(self):
         self.cycles += 4
 
+
+'''
+Clase CPU
+    - Atributos: PC, ACC, IR, MDR, MAR, Memory
+    - Metodos:
+        - loadProgram(code)
+        - fetch()
+        - execute()
+'''
 class CPU:
     def __init__(self):
         
-        #Registers PC, ACC, IR, MDR, MAR
-        self.PC = 0
-        self.ACC = 0
-        self.IR = 0
-        self.MDR = 0
-        self.MAR = 0
+  #Registers PC, ACC, IR, MDR, MAR
+        self.PC = 0     # contador de programa
+        self.ACC = 0    # Acumulador -> resultados -> intermedio
+        self.IR = 0     # Registro de Instrucciones
+        self.MDR = 0    # Datos de memoria
+        self.MAR = 0    # direcciones de memoria
 
-        #RAM or self.memory, 6 bit address bus --> 2^6 = 64 bytes of RAM
+        #RAM or self.memory, 6 bit address 
+        #bus --> 2^6 = 64 bytes of RAM
         self.memory = [0]*64
 
         #Flags
-        self.HLT = False
-        self.clock = CLOCK()
+        self.HLT = False        # Atributo detener
+        self.clock = CLOCK()    # instancia Reloj
 
     def loadProgram(self, code):
         self.memory = list(code)  
 
     def fetch(self):
-        #fetch instruction
+        #fetch instruction  -> busca instruccion
         print(f"------------------memory[{self.PC}]-------------------------")
         self.MAR = self.PC
         self.clock.CountCycles()
         self.MDR = self.memory[self.MAR]
-        self.PC += 1
+        self.PC += 1            # suma una instruccion
         self.clock.CountCycles()       
          #wait memory
         self.clock.WaitCycles4()
@@ -65,7 +82,7 @@ class CPU:
             self.clock.CountCycles()       
             print(f"executing LDA {self.MAR}    => ACC({self.ACC}) ← memory[{self.MAR}]")
            
-        elif(opcode == 0x1):
+        elif(opcode == 0x1):   
             #STA
             self.MAR = address
             self.clock.CountCycles()       
@@ -76,7 +93,7 @@ class CPU:
             #wait memory
             self.clock.WaitCycles4()
             print(f"executing STA {self.MAR}    => memory[{self.MAR}] ← ACC({self.ACC})")
-        elif(opcode == 0x2):
+        elif(opcode == 0x2):   
             #ADD
             self.MAR = address
             self.clock.CountCycles() 
@@ -87,7 +104,7 @@ class CPU:
             print(f"executing ADD {self.MAR}    => ACC({self.ACC + self.MDR}) ← ACC({self.ACC}) + MDR({self.MDR})")
             self.ACC = self.ACC + self.MDR 
             self.clock.CountCycles()            
-        elif(opcode == 0x3):
+        elif(opcode == 0x3):   
             #HLT
             self.HLT = True
             self.clock.CountCycles()
@@ -96,6 +113,10 @@ class CPU:
             print(f"Illegal opcode {hex(opcode)}")
 
 
+'''
+Main 
+    - Entra en un bucle infinito hasta encontrar una instruccion HLT
+'''
 def main(filename):
     cpu = CPU()
     asm = ASSEMBLY()
@@ -103,7 +124,7 @@ def main(filename):
     asm.assembler(filename)
     cpu.loadProgram(asm.code)
 
-    while not cpu.HLT:
+    while not cpu.HLT: 
         
         try:
             #stage fetch
@@ -115,11 +136,16 @@ def main(filename):
         except Exception as e:
             print("HALTING System...")
             break;
+
+    '''
+    Solucion propuesta por Christiam         
+    '''
+
     print(" ---------------------------------------------------")
     print(f"Program-Cycles: {cpu.clock.cycles}")
-    print("RI: ?")
-    print("CPI: ?")
-    print("Time CPU: ?")
+    print(f"      RI: {cpu.PC}" )
+    print(f"     CPI: {cpu.clock.cycles / cpu.PC}" )   
+    print(f"Time CPU: {cpu.clock.cycles * cpu.PC * cpu.clock.cycles / cpu.PC}  ")
     print(" -------------------------------------------")
 
 if __name__ == "__main__":
